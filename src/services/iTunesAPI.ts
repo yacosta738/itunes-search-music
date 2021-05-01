ff511b8cead230f85eed895859a0269164348662
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-05-01 16:25:48 -0400

    CORS HEADERS

--- src/services/iTunesAPI.ts
+++ src/services/iTunesAPI.ts
@@ -2,5 +2,11 @@ import { ItunesTypes } from "../types/ItunesTypes.interface";
 
 export const itunesSearch = async (search: string): Promise<ItunesTypes> =>
   await fetch(
-    `https://itunes.apple.com/search?term=${search}&entity=album`, {mode:'cors'}
+    `https://itunes.apple.com/search?term=${search}&entity=album`,   {
+        headers: [
+          ["Content-Type", "application/json"],
+          ["Content-Type", "text/plain"]
+        ],
+        credentials: "include",
+      }
   ).then(val => val.json());
